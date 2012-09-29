377ea13f1e0894159bff51ed6eddccef8fe3d9a3
Artem Ablovatskiy <single-minded@tut.by>
2012-09-29 16:27:38 +0300

    Отображение следующей пары на главном экране.

--- FKSiS-Schedule/src/by/fksis/schedule/app/DayScheduleFragment.java
+++ FKSiS-Schedule/src/by/fksis/schedule/app/DayScheduleFragment.java
@@ -2,6 +2,7 @@ package by.fksis.schedule.app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +20,18 @@ import com.ormy.Application;
 import com.ormy.DatabaseObserver;
 import com.ormy.Model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 public class DayScheduleFragment extends Fragment implements DatabaseObserver {
     private Calendar date;
     private int weekNumber, dayOfWeek;
+    private DateFormat sdf_all = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+    private DateFormat sdf_date = new SimpleDateFormat("yyyy-MM-dd");
 
     public DayScheduleFragment(Calendar date) {
         this.date = date;
@@ -55,6 +62,15 @@ public class DayScheduleFragment extends Fragment implements DatabaseObserver {
 
     public void refresh() {
         broadcastContainer.removeAllViews();
+        currentClass.setText(getString(R.string.no_classes));
+        nextClass.setText(getString(R.string.no_classes));
+
+        Calendar time = Calendar.getInstance();
+        time.setTimeInMillis(System.currentTimeMillis());
+        time.set(Calendar.HOUR, 0);
+        time.set(Calendar.MINUTE, 00);
+        time.set(Calendar.AM_PM, Calendar.AM);
+        time.add(Calendar.DATE, 1);
 
         List<Broadcast> broadcasts = Broadcast.get(Broadcast.class)
                 .filter("groupList%", "%" + new Preferences(getActivity()).getGroup() + "%")
@@ -77,6 +93,7 @@ public class DayScheduleFragment extends Fragment implements DatabaseObserver {
                 .filter("studentGroup", new Preferences(getActivity()).getGroup())
                 .filter("subgroups%", "%" + new Preferences(getActivity()).getSubgroupString() + "%")
                 .list();
+        boolean found = false;
         for (ScheduleClass clazz : classes) {
             View lineView = LayoutInflater.from(getActivity()).inflate(R.layout.day_schedule_line, container, false);
             LineViewHolder holder = new LineViewHolder();
@@ -88,6 +105,43 @@ public class DayScheduleFragment extends Fragment implements DatabaseObserver {
             holder.time2.setText(getResources().getStringArray(R.array.timeSlotEnd)[clazz.timeSlot]);
             holder.room.setText(clazz.room);
             container.addView(lineView);
+            Date dateStart = null ,dateEnd = null;
+            try {
+                dateStart = sdf_all.parse(sdf_date.format(Calendar.getInstance().getTime()) + " " + getResources().getStringArray(R.array.timeSlotStart)[clazz.timeSlot]);
+                dateEnd = sdf_all.parse(sdf_date.format(Calendar.getInstance().getTime()) + " " + getResources().getStringArray(R.array.timeSlotEnd)[clazz.timeSlot]);
+            } catch (Exception e){
+                Log.e(DayScheduleFragment.class.getSimpleName(),e.getMessage());
+            }
+            if ((dateStart.getTime() <= Calendar.getInstance().getTime().getTime())
+                    && (dateEnd.getTime() >= Calendar.getInstance().getTime().getTime())) {
+                currentClass.setText(clazz.name + " " + ((clazz.room != null) ? clazz.room : ""));
+                time.setTime(dateEnd);
+                if ((classes.size() -1)  < classes.lastIndexOf(clazz))  {
+                    ScheduleClass l_next = classes.get(classes.lastIndexOf(clazz) +1);
+                    nextClass.setText( l_next.name + l_next.room);
+                } else {
+                    nextClass.setText(getString(R.string.no_classes));
+                }
+                found = true;
+            }
+        }
+        if (!found) {
+            found = false;
+            for (Iterator<ScheduleClass> i = classes.iterator(); i.hasNext() && !found; ) {
+                ScheduleClass l = i.next();
+                Date dateStart = null, dateEnd;
+                try {
+                    dateStart = sdf_all.parse(sdf_date.format(Calendar.getInstance().getTime()) + " " + getResources().getStringArray(R.array.timeSlotStart)[l.timeSlot]);
+                } catch (Exception e){
+                    Log.e(DayScheduleFragment.class.getSimpleName(),e.getMessage());
+                }
+                if (dateStart.getTime() > Calendar.getInstance().getTime().getTime()) {
+                    found = true;
+                    currentClass.setText(getString(R.string.no_classes));
+                    nextClass.setText(l.name + " " + ((l.room != null) ? l.room : ""));
+                    time.setTime(dateStart);
+                }
+            }
         }
     }
 
@@ -103,6 +157,12 @@ public class DayScheduleFragment extends Fragment implements DatabaseObserver {
 
     @InjectView(R.id.broadcast_container)
     private LinearLayout broadcastContainer;
+
+    @InjectView(R.id.currentClass)
+    private TextView currentClass;
+
+    @InjectView(R.id.nextClass)
+    private TextView nextClass;
 
     @Override
     public void databaseObjectUpdated(Model<?> model) {
