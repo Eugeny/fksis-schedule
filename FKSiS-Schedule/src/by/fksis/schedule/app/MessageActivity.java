c502cc8523dedcf89608134ce57838081bb9e54d
antonsapognikov <anton.sapognikov@gmail.com>
2012-09-29 18:07:39 +0200

    Group Monitor API

--- FKSiS-Schedule/src/by/fksis/schedule/app/MessageActivity.java
+++ FKSiS-Schedule/src/by/fksis/schedule/app/MessageActivity.java
@@ -1,16 +1,16 @@
 package by.fksis.schedule.app;
 
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import by.fksis.schedule.API;
 import by.fksis.schedule.R;
 import com.WazaBe.HoloEverywhere.Toast;
-import com.WazaBe.HoloEverywhere.View;
 import com.WazaBe.HoloEverywhere.sherlock.SActivity;
 import com.danikula.aibolit.Aibolit;
 import com.danikula.aibolit.annotation.InjectView;
 import com.danikula.aibolit.annotation.InjectOnClickListener;
+import java.util.Date;
 
 public class MessageActivity extends SActivity {
     @Override
@@ -28,6 +28,16 @@ public class MessageActivity extends SActivity {
 
     @InjectOnClickListener(R.id.message_btn)
     public void onClick(android.view.View v) {
-        Toast.makeText(this, "Test", android.widget.Toast.LENGTH_LONG).show();
+        Date date = new Date(dateText.getYear() - 1900, dateText.getMonth(), dateText.getDayOfMonth());
+        if (editText.getText().toString().equals("") || date.before(new Date())) {
+            Toast.makeText(this, getString(R.string.invalid_message), android.widget.Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            API.queryAddBroadcasts(editText.getText().toString(), Long.toString(date.getTime()));
+        }
+        catch (Exception e) {
+            Toast.makeText(this, getString(R.string.connection_error), android.widget.Toast.LENGTH_SHORT).show();
+        }
     }
 }
